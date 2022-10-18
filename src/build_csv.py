44216fb9931824f169d45a25a09ce0449174d9f7
Victor Castaneda <victor.castaneda@hey.net.co>
2022-10-18 15:55:46 -0500

    Task 1257: Basic csv file

--- src/build_csv.py
+++ src/build_csv.py
@@ -3,8 +3,11 @@ import json
 import logging
 import os
 
-import boto3
 from dotenv import load_dotenv
+import awswrangler as wr
+import pandas as pd
+import boto3
+import pg8000.native
 
 load_dotenv()
 
@@ -14,13 +17,35 @@ logger.setLevel(logging.INFO)
 
 class BuildCsv:
     def __init__(self):
-        self.s3_client = boto3.client(
-            service_name="s3",
-            region_name=os.getenv("REGION"),
-            aws_access_key_id=os.getenv("ACCESS_KEY"),
-            aws_secret_access_key=os.getenv("SECRET_KEY")
-        )
         self.now = datetime.now()
 
-    def get_data(self):
-        pass
+    @staticmethod
+    def get_dataframe():
+        conn = wr.postgresql.connect(
+            secret_id=str(os.getenv("SECRET_ARN"))
+        )
+        df = wr.postgresql.read_sql_query(
+            sql="SELECT email FROM user_company LIMIT 100;",
+            con=conn
+        )
+        return df
+
+    @staticmethod
+    def update_data(dataframe):
+        dataframe['FECHA_ENVIO'] = ''
+        dataframe['FECHA_ACTIVIDAD'] = ''
+        dataframe['CAMPANIA'] = ''
+        dataframe['ACCION'] = ''
+        dataframe['TIPO_ACCION'] = ''
+        dataframe['ACTIVIDAD'] = ''
+        dataframe['DESCRIPCION'] = ''
+        dataframe['TAG'] = ''
+        return dataframe
+
+    def handler(self):
+        print(self.update_data(dataframe=self.get_dataframe()))
+
+
+if __name__ == '__main__':
+    build_csv = BuildCsv()
+    print(build_csv.handler())
